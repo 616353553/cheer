83dd985c22a613d2bb1796530fb2ff4cd15d00e8
616353553 <xiaobai1993124>
2016-10-14 02:04:01 -0500

    Initial Commit

---
+++ cheer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cheer
+//
+//  Created by bainingshuo on 16/10/14.
+//  Copyright © 2016年 Evolvement Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
